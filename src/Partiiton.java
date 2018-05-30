17c7a6218d6f934f883a14ea7637e9b377545a39
황선아 <hwangseon-a@hwangseon-as-MacBook-Pro.local>
2018-05-30 23:30:47 +0900

    2-4 분할

--- src/Partiiton.java
+++ src/Partiiton.java
@@ -1,5 +1,38 @@
+import LinkedList.LinkedListNode;
 
 public class Partiiton {
+	public static class LinkedListNode{
+		LinkedListNode next = null;
+		int data;
+		public LinkedListNode(int d) {
+			data = d;
+		}
+		void appendToTail(int d) {
+			LinkedListNode end = new LinkedListNode(d);
+			LinkedListNode n = this;
+			while(n.next != null) {
+				n = n.next;
+			}
+			n.next = end;
+				
+			
+		}
+	}
+	LinkedListNode partition(LinkedListNode node,int x) {
+		LinkedListNode beforeStart  = null;
+		LinkedListNode beforeEnd = null;
+		LinkedListNode afterStart = null;
+		LinkedListNode atferend = null;
+		
+		while(node != null) {
+			LinkedListNode next = node.next;
+			node.next = null;
+			
+		}
+	}
+
+	
+			
 
 	public static void main(String args[]) {
 		
