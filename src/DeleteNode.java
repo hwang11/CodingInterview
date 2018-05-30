94d10076cd856a7374d954a3ce043662cb2b517e
황선아 <hwangseon-a@hwangseon-as-MacBook-Pro.local>
2018-05-30 23:04:29 +0900

    2-3 중간 노드 삭제

---
+++ src/DeleteNode.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class DeleteNode {
+	public static class LinkedListNode{
+		LinkedListNode next = null;
+		int data;
+		public LinkedListNode(int d) {
+			data = d;
+		}
+		void appendToTail(int d) {
+			LinkedListNode end = new LinkedListNode(d);
+			LinkedListNode n = this;
+			while(n.next != null) {
+				n = n.next;
+			}
+			n.next = end;		
+		}
+	}
+	boolean deleteNode(LinkedListNode n) {
+		if(n == null|| n.next == null) {
+			return false;
+		}
+		LinkedListNode next = n.next;
+		n.data = next.data;
+		n.next = next.next;
+		return true;
+	}
+	
+	public static void main(String args[]) {
+		DeleteNode d = new DeleteNode();
+		LinkedListNode n = new LinkedListNode(30);
+		n.appendToTail(10);//data값 
+		n.appendToTail(20);
+		n.appendToTail(30);
+		d.deleteNode(n.next);
+		while(n!=null) {
+			System.out.println(n.data);
+			n = n.next;
+		}
+	}
+}
---
+++ src/Partiiton.java
@@ -0,0 +1,7 @@
+
+public class Partiiton {
+
+	public static void main(String args[]) {
+		
+	}
+}
